f3466ee87ccd0a0af8f44675320b30da0477a76c
Siripron Thammarakkitanon <>
2019-09-14 00:59:55 +0700

    commit detail

--- app/src/main/kotlin/scb/academy/jinglebell/activity/DetailProfileActivity.kt
+++ app/src/main/kotlin/scb/academy/jinglebell/activity/DetailProfileActivity.kt
@@ -1,13 +1,30 @@
 package scb.academy.jinglebell.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_detail_profile.*
 import scb.academy.jinglebell.R
+import scb.academy.jinglebell.model.Song
 
 class DetailProfileActivity : AppCompatActivity() {
+
+
+    companion object {
+        const val EXTRA_KEY_TITLE = "TITLE_NAME"
+
+
+        fun startActivity(context: Context?, titleName: String) =
+            context?.startActivity(Intent(context, DetailProfileActivity::class.java).also { intent ->
+                intent.putExtra(EXTRA_KEY_TITLE, titleName)
+            })
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_profile)
+        txtName.text = intent.getStringExtra(EXTRA_KEY_TITLE)
     }
 }
--- app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
+++ app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
@@ -1,27 +1,33 @@
 package scb.academy.jinglebell.fragment
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_profile.*
 import scb.academy.jinglebell.R
+import scb.academy.jinglebell.activity.DetailProfileActivity
 
-class ProfileFragment : Fragment(){
-
-
-//    private fun setView() {
-//        btnrefresh.setOnClickListener { presenter.getBeerApi() }
-//    }
+class ProfileFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_profile ,container , false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        txtName.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+            DetailProfileActivity.startActivity(context ,txtName.text.toString() )
+                return@OnKeyListener true
+            }
+            false
+        })
+
     }
+
 }
